59eebd2ee91157442cc632ad5c0a98abca16b943
Jiri Korinek <kormaister@gmail.com>
2016-03-16 15:56:55 +0100

    Initial Commit

---
+++ Assets/Editor/WorldEditorEditor.cs
@@ -0,0 +1,68 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(WorldEditor))] // declares that this is a custom editor script
+[System.SerializableAttribute]
+public class WorldEditorEditor : Editor //inherits from the editor class to do all the good stuff
+{
+
+    [SerializeField] private Object prefab;
+    private bool inEditMode = false;
+    private WorldEditor worldEditor; 
+
+    /// <summary>
+    /// what happens when script gets enabled
+    /// </summary>
+    void OnEnable()
+    {
+        
+    }
+
+    /// <summary>
+    /// Function that overrides the normal editor with a custom one we are going to make
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        prefab = EditorGUILayout.ObjectField("Prefab To Paint", prefab, typeof (GameObject));
+    }
+
+    /// <summary>
+    /// Function that handles all the input in the actual scene window
+    /// </summary>
+    void OnSceneGUI()
+    {
+        //Check for edit mode
+        if (Event.current.alt)
+        {
+            inEditMode = false;
+        }
+        else
+        {
+            inEditMode = true;
+        }
+        // if in edit mode, start processing inputs
+        if (inEditMode)
+        {
+            //Disable mouse selection while in edit mode to prevent selection
+            if (Event.current.type == EventType.layout)
+            {
+                HandleUtility.AddDefaultControl(GUIUtility.GetControlID(GetHashCode(), FocusType.Passive));
+            }
+
+            if (Event.current.type == EventType.mouseDown)
+            {
+                Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition); //cast ray from screen point towards 3d space - super useful
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(ray, out hit, 1000f))
+                {
+                    //if we hit terrain, execute prop instantiation.
+                    Vector3 brushPosition = hit.point;
+                    worldEditor = (WorldEditor) target;
+                    worldEditor.PrefabBrush(brushPosition,(GameObject)prefab);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/WorldEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode] //Enables command execution in edit mode
+public class WorldEditor : MonoBehaviour {
+
+    /// <summary>
+    /// Simple function to instantiate an object on a position - will be used together with our custom editor script
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="prefab"></param>
+    public void PrefabBrush(Vector3 position, GameObject prefab)
+    {
+        GameObject spawnedObject = Instantiate(prefab);
+        spawnedObject.transform.position = position;
+    }
+
+}
